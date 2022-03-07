e0c08afc626ac46a4f4de335535c0aa459527588
Carlos Loureda <carloslouredaparrado@gmail.com>
2022-03-07 20:06:19 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello MyReads</h1>
+      <h2>Aquí vamos a hacer una app de ejemplo</h2>
+    </div>
+  );
+}
